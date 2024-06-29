c3a20604113e7016c4d211dc5d07d0a2bcc6f2b1
Luis Enrique Ramírez <luian.ramirez.12@gmail.com>
2024-06-29 08:59:17 -0600

    chore: Update .gitignore and add new constants in messages.go

--- backend/internal/core/domain/constants/messages.go
+++ backend/internal/core/domain/constants/messages.go
@@ -2,6 +2,7 @@ package constants
 
 const (
 	REQUEST_INVALID        = "Invalid request"
+	REQUEST_UNAUTHORIZED   = "Unauthorized request"
 	REQUEST_ERROR          = "Error processing request"
 	REQUEST_SUCCESS        = "Request processed successfully"
 	REQUEST_USER_NOT_FOUND = "User not found"
